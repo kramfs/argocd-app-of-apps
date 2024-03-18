fe66aea31b7a18591b18272df5e825425d40ac37
Rudel Saldivar <rudelsaldivar@gmail.com>
2024-03-18 10:05:11 +0800

    Removed unused projects resource

--- argocd/main.auto.tfvars
+++ argocd/main.auto.tfvars
@@ -3,7 +3,7 @@ minikube = {
   driver             = "docker" # Options: docker, podman, kvm2, qemu, hyperkit, hyperv, ssh
   kubernetes_version = "v1.28.3"  # See available options: "minikube config defaults kubernetes-version" or refer to: https://kubernetes.io/releases/
   container_runtime  = "containerd" # Options: docker, containerd, cri-o
-  nodes              = "1"
+  nodes              = "2"
 }
 
 # METALLB
@@ -26,9 +26,9 @@ argocd = {
   namespace        = "argocd"
   create_namespace = true
 
-  repository      = "https://argoproj.github.io/argo-helm"
-  chart           = "argo-cd"
-  #version        = "4.9.1"               # Chart version
-  server_service_type  = "LoadBalancer"   # Values: ClusterIP, LoadBalancer, NodeIP
-  timeout_reconciliation = "10s"          # Default: 180s. Reconciliation timeout (drift) how often it will sync ArgoCD with the Git repository.
+  repository              = "https://argoproj.github.io/argo-helm"
+  chart                   = "argo-cd"
+  #version                = "4.9.1"               # Chart version
+  server_service_type     = "LoadBalancer"   # Values: ClusterIP, LoadBalancer, NodeIP
+  timeout_reconciliation  = "10s"          # Default: 180s. Reconciliation timeout (drift) how often it will sync ArgoCD with the Git repository.
 }
--- argocd/main.tf
+++ argocd/main.tf
@@ -8,7 +8,7 @@ module "minikube_cluster" {
   driver              = lookup(var.minikube, "driver", "docker")                 # # Options: docker, podman, kvm2, qemu, hyperkit, hyperv, ssh
   kubernetes_version  = lookup(var.minikube, "kubernetes_version", null)         # See options: "minikube config defaults kubernetes-version" or refer to: https://kubernetes.io/releases/
   container_runtime   = lookup(var.minikube, "container_runtime", "containerd")  # Default: containerd. Options: docker, containerd, cri-o
-  nodes               = lookup(var.minikube, "nodes", "1")
+  nodes               = lookup(var.minikube, "nodes", "2")
 }
 
 
@@ -61,11 +61,11 @@ resource "helm_release" "metallb" {
   chart      = var.metallb.chart
   version    = lookup(var.metallb, "version", null) # Chart version
 
-  values = [
-    templatefile("./helm_values/metallb-system.yaml", {
-      serviceMonitor_enabled = lookup(var.metallb, "serviceMonitor_enabled", false) # Check if servicemonitor will be enabled
-    })
-  ]
+  #values = [
+  #  templatefile("./helm_values/metallb-system.yaml", {
+  #    serviceMonitor_enabled = lookup(var.metallb, "serviceMonitor_enabled", false) # Check if servicemonitor will be enabled
+  #  })
+  #]
 
   provisioner "local-exec" {
     command = "kubectl apply -f ./helm_values/metallb-system-config.yaml"
